3c2b97d6fa0694d01f8d776e6d4ab60938319448
SahilKishor21 <sahil.21ug4021@iiitranchi.ac.in>
2025-09-17 00:36:22 +0530

    Web Socket fixed

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     domains: ['localhost'],
   },
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001',
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001' || 'https://elevate-x-seven.vercel.app/',
   },
 }
 
